13f524cfc91717c8ffec503f18bc5abb54ad4efa
AbstractedLayer <abstractedlayer@gmail.com>
2022-01-29 18:34:42 -0500

    Simplifying how nvm is installed - fail if python is not installed

--- scripts/installers/nvm.sh
+++ scripts/installers/nvm.sh
@@ -2,36 +2,19 @@
 
 if [[ -n "$INCLUDE_NVM" && -n "$NVM_VER" ]]
 then
-  source $HOME/.profile
-
-  if [[ -z "$PYENV_VER" ]]
+  if [[ -z "$PYENV_VER" || -z "$PYTHON_VER" ]]
   then
-    PYENV_VER=v2.2.4
-    echo "Defaulting PYENV_VER to $PYENV_VER for node install"
+    echo "Cannot install NVM without python already installed (PYENV_VER: $PYENV_VER, PYTHON_VER: $PYTHON_VER)"
+    exit 1
   fi
-
-  if [[ -z "$PYTHON_VER" ]]
-  then
-    PYTHON_VER=3.10.2
-    echo "Defaulting PYTHON_VER to $PYENV_VER for node install"
-  fi
-
-  if ! command -v python &> /dev/null
-  then
-    echo "Installing python for Node"
-    INCLUDE_PYENV=1 PYENV_VER=$PYENV_VER PYTHON_VER=$PYTHON_VER sh -c ./pyenv.sh
-  fi
-
-
 
   echo "Installing NVM $NVM_VER"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VER/install.sh | bash
+  sh -cl "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VER/install.sh | bash"
 
   if [[ -n "$NODE_VER" ]]
   then
     echo "Installing node $NODE_VER"
-    source $HOME/.profile
-    nvm install -s $NODE_VER
+    sh -cl "nvm install -s $NODE_VER && nvm cache clear"
   else
     echo "Skipping installation of default node version"
   fi
